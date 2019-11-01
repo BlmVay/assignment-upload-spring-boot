94a13bd2a18767584caa97c99d7bd343e9e631ea
914313670 <914313670@qq.com>
2019-11-01 21:01:21 +0800

    1

--- src/main/java/top/liujingyanghui/assignmentupload/controller/CityController.java
+++ src/main/java/top/liujingyanghui/assignmentupload/controller/CityController.java
@@ -1,17 +1,36 @@
 package top.liujingyanghui.assignmentupload.controller;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import top.liujingyanghui.assignmentupload.dao.CityMapper;
+import top.liujingyanghui.assignmentupload.entity.City;
+import top.liujingyanghui.assignmentupload.service.CityService;
+import top.liujingyanghui.assignmentupload.vo.Result;
 
-
+/**
+ * 城市
+ */
 @RestController
 @RequestMapping("api/city")
 public class CityController {
 
     @Autowired
-    private CityMapper cityMapper;
+    private CityService cityService;
 
-
+    /**
+     * 根据省份获取城市列表
+     *
+     * @return
+     */
+    @GetMapping("list")
+    public Result list(@RequestParam Integer provinceId) {
+        if (-1 == provinceId) {
+            return Result.success(cityService.list());
+        } else {
+            return Result.success(cityService.list(Wrappers.<City>lambdaQuery().eq(City::getProvinceId, provinceId)));
+        }
+    }
 }
--- src/main/java/top/liujingyanghui/assignmentupload/controller/ProvinceController.java
+++ src/main/java/top/liujingyanghui/assignmentupload/controller/ProvinceController.java
@@ -1,8 +1,28 @@
 package top.liujingyanghui.assignmentupload.controller;
 
-/**
- *
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.liujingyanghui.assignmentupload.service.ProvinceService;
+import top.liujingyanghui.assignmentupload.vo.Result;
 
+/**
+ * 省份
+ */
+@RestController
+@RequestMapping("api/province")
 public class ProvinceController {
+
+    @Autowired
+    private ProvinceService provinceService;
+
+    /**
+     * 获取省份列表
+     * @return
+     */
+    @GetMapping("list")
+    public Result list() {
+        return Result.success(provinceService.list());
+    }
 }
